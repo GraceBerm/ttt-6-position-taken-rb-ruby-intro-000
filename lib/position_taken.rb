6ccb297e0d3208a68c6b03b0c389351f7de80299
Grace Berman <grace.g.berman@gmail.com>
2019-12-14 13:47:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+index = 0
 
 def position_taken?(board, index)
   board[index] == "  " && board[index] == " " && board[index] == nil && board[index] != ("X" || "O")
 end
-
-board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
-index = 0
