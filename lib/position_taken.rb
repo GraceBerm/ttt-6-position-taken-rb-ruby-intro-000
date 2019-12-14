c74142f01a1f5be5fb1ac52640feeef775ae0e55
Grace Berman <grace.g.berman@gmail.com>
2019-12-14 13:29:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,5 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def position_taken?(board, index)
-  if array[index_number] == " " || array[index_number] == ""
+  array[index_number] == " " || array[index_number] == ""
 end
