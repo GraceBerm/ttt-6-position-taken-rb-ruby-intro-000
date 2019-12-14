1e955edd4e4a1c072778c07059c8720d435c3000
Grace Berman <grace.g.berman@gmail.com>
2019-12-14 14:02:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,11 @@
 
 
 def position_taken?(board, index)
-board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+if board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
   index = 0
+else board = [" ", " ", " ", " ", " ", " ", " ", " ", ""]
+  index = 0
+end
   board[index] == "  "
   board[index] == " "
   board[index] == nil
