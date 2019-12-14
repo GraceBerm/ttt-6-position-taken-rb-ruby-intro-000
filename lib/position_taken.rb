2ae6663bb6a21855c12b908b0016030a0ab5178f
Grace Berman <grace.g.berman@gmail.com>
2019-12-14 14:08:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,5 +4,6 @@ def position_taken?(board, index)
   if board[index] == "  " || board[index] == " " || board[index] == nil
     return false
   else board[index] == "X" || "O"
-    return true  
+    return true
 end
+end 
